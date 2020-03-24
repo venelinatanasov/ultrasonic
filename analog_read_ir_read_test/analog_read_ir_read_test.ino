d16e8f71bdd874a98bd315cbd79abd12081eb596
Venelin Atanasov <venelinatanasov@pop-os.localdomain>
2020-03-24 17:35:48 +0200

    further analog ir read development

--- analog_read_ir_read_test/analog_read_ir_read_test.ino
+++ analog_read_ir_read_test/analog_read_ir_read_test.ino
@@ -1,1 +1,35 @@
+#define analog A0
+#define led 11
+#define IRtolerance 0.2
+int all;
+void setup(){
+   pinMode(analog, INPUT);
+   pinMode(led,OUTPUT);
+    Serial.begin(9600);
+   all=calibrate();
+}
 
+void loop(){
+  
+  Serial.println(all);
+  int irsig=analogRead(analog);
+  if(irsig>(all+IRtolerance*all)){
+   digitalWrite(led,HIGH); 
+  }else{
+    digitalWrite(led,LOW);
+  }
+  
+}
+
+int calibrate(){
+  int a;
+  unsigned long all=0;
+ for(int i=0;i<100;i++){
+   a=analogRead(A0);
+   all+=a;
+ }
+ all=all/100;
+ return all;
+ 
+  
+}
--- receiver/receiver/receiver.ino
+++ receiver/receiver/receiver.ino
@@ -1,13 +1,16 @@
 #define irsig 10
 #define trig 8
 #define echo 9
-
+#define led 11
+#define IRtolerance 0.2
+int all;
 
 void setup() {
   pinMode(irsig, INPUT);
   pinMode(trig, OUTPUT);
   pinMode(echo, INPUT);
   Serial.begin(9600);
+  all=calibrate();
 
 }
 
@@ -16,7 +19,10 @@ void loop() {
   unsigned long t;
   float dist;
   while(digitalRead(irsig)==HIGH){}
-  while(digitalRead(irsig)==LOW){}
+  unsigned long x=millis();
+  while(digitalRead(irsig)==LOW){
+    if(millis()-x>1000){all=calibrate();return;}
+  }
   digitalWrite(trig, HIGH);
   delayMicroseconds(10);
   digitalWrite(trig, LOW);
@@ -28,4 +34,16 @@ void loop() {
   Serial.println(dist);
   
 
+}
+int calibrate(){
+  int a;
+  unsigned long all=0;
+ for(int i=0;i<100;i++){
+   a=analogRead(A0);
+   all+=a;
+ }
+ all=all/100;
+ return all;
+ 
+  
 }
